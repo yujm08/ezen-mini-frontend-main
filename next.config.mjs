844022946829c91a61ee6c60dc833632ace1cec1
yujm08 <08yujm@gmail.com>
2025-01-06 12:41:54 +0900

    GitHub Actions 타임아웃 연장, 빌드 시 ESLint 검사 무시

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: './.next', // 빌드 결과물 경로 지정
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 중 ESLint 검사 비활성화
+  },
+  experimental: {
+    appDir: true, // src/app 디렉토리 활성화
+  },
 };
 
 export default nextConfig;
-
-  
-  
-  
