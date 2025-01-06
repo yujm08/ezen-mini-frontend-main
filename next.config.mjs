1ce0e3d29301c858fe48210dff9646bf205db751
yujm08 <08yujm@gmail.com>
2025-01-06 14:17:32 +0900

    EC2 재부팅

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   distDir: './.next', // 빌드 결과물 경로 지정
   eslint: {
-    ignoreDuringBuilds: true, // 빌드 중 ESLint 검사 비활성화
+    ignoreDuringBuilds: true, // (CICD)빌드 중 ESLint 검사 비활성화
   },
   experimental: {
     appDir: true, // src/app 디렉토리 활성화
